39612840ed9567023c83a9a8801986c65b35cc92
SOUMYA MUKHERJEE <soumyamukherjee910@gmail.com>
2022-12-10 00:42:31 +0530

    Create 203. Remove Linked List Elements.cpp

---
+++ 203. Remove Linked List Elements.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+         ListNode *pseudo_head = new ListNode(0);
+        pseudo_head->next = head;
+        ListNode *cur = pseudo_head;
+        while(cur){
+            if(cur->next && cur->next->val == val)   cur->next = cur->next->next;
+            else    cur = cur->next;
+        }
+        return pseudo_head->next;
+        
+    }
+};
