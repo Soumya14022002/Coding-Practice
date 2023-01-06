82c14829e7a261baccb48f66db9474c0687aaa92
SOUMYA MUKHERJEE <soumyamukherjee910@gmail.com>
2023-01-06 10:42:37 +0530

    Create 35. Search Insert Position.cpp

---
+++ 35. Search Insert Position.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) 
+    {
+       
+        int n=nums.size();
+        int start=0 , end=n-1;
+        int ans=0;
+        while(start<=end)
+        {
+            int mid=start+((end-start)/2);
+            if(nums[mid]<target)
+            {
+                start=mid+1;
+                ans=start;
+            }
+            else
+            {
+                end=mid-1;
+            }
+        }
+        return ans;
+        
+    }
+};
